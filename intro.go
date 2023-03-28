11f89e7a8f5ee1c1855ae0e72650336237c86913
otobongfp <otobongfp@gmail.com>
2023-03-28 21:52:37 +0100

    some changes

--- intro.go
+++ intro.go
@@ -13,6 +13,18 @@ func main() {
 		"DB": {name: "Dan Brown"},
 	}
 
-	fmt.Printf("%#v \n", persons)
+	fmt.Printf("%v \n", persons)
+	fmt.Printf("DB = %v \n", persons["DB"])
+
+	//change value
+	persons["DB"] = person{name: "Joseph Smith"}
+
+	value, ok := persons["DB"]
+	fmt.Printf("DB = %v, ok = %v \n", value, ok)
+
+	delete(persons, "DB")
+	fmt.Printf("DB = %v, ok = %v \n", value, ok)
+	fmt.Printf("DB = %v \n", persons["DB"])
+	fmt.Printf("%v \n", persons)
 
 }
