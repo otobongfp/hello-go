5c85fff2872227bba5b08506f26c0f36b4d967a8
otobongfp <otobongfp@gmail.com>
2023-03-28 20:42:26 +0100

    some changes

--- intro.go
+++ intro.go
@@ -3,14 +3,34 @@ package main
 import "fmt"
 
 type author struct {
-	name, book string
+	first string
+	last  string
+}
+
+type person struct {
+	author
+	penName string
+}
+
+func (value author) fullName() string {
+	return value.first + " " + value.last
+}
+
+func (value person) fullName() string {
+	return fmt.Sprintf("%s , (%s)", value.author.fullName(), value.penName)
 }
 
 func main() {
-	me := author{
-		"Otobong",
-		"Book of Life",
+
+	me := person{
+		author{
+			"Otobong",
+			"Peter",
+		},
+		"Bolt",
 	}
 
+	fmt.Println(me.fullName())
 	fmt.Printf("%#v \n", me)
+
 }
