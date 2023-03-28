19d56f029c9f38d0d209cc1cd551b1f98f50f268
otobongfp <otobongfp@gmail.com>
2023-03-28 01:18:08 +0100

    some changes

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example/hello
 go 1.20
 
 require (
+	github.com/jboursiquot/go-proverbs v0.0.2 // indirect
 	golang.org/x/mod v0.9.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
--- intro.go
+++ intro.go
@@ -1,50 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+var val = "global"
+
+func changeVal() {
+	fmt.Println("global val = ", val)
+}
+
+func updateVal() {
+	val = "updated global"
+	fmt.Println("global val = ", val)
+}
 
 func main() {
 
-	//Write a program to determine how fast a ship would need to travel (in km/h)
-	//in order to reach Malacandra in 28 days. Assume a distance of 56,000,000 km.
-
-	//Speed = Distance/Time (km/hr)
-	// var t = 28 * 24   //converts days to hrs
-	// var d = 56000000  //Km
-	// var speed = d / t // Km/hr
-
-	// fmt.Println(speed)
-
-	//or method 2
-
-	// t, d, speed := 28*24, 56000000, 0
-	// speed = d / t
-	// fmt.Println(speed)
-
-	// var command = "Get Free"
-	// exit := strings.Contains(command, "Free")
-	// fmt.Println("You are free now!!!", exit)
-	//------------------------------------------------
-
-	// COMPARISONS
-	// var age = 41
-	// var toDrink = age > 18
-	// fmt.Printf("I am %v, am I eligible for a drink? %v \n", age, toDrink)
-	//------------------------------------------------
-	//CALCULATE THE LEAP YEARS
-	// fmt.Println("Is 2028 a Leap Year?")
-	// var leap = 2028/400 == 0 || 2028%4 == 0
-	// if leap {
-	// 	fmt.Println("Yes it is a leap year")
-	// } else {
-	// 	fmt.Println("No it is Not!!!")
-	// }
-
-	var haveTorch = true
-	var litTorch = false
-	if !haveTorch || !litTorch {
-		fmt.Println("Nothing to see here.")
-	}
-
+	val := 42
+	//a = *val
+	fmt.Printf("%T, local val = %v \n", val, val)
+	changeVal()
+	updateVal()
+	fmt.Printf("%T, local val = %v \n", &val, &val)
 }
