3e9301552fc04919d177c2d9d26e58b918ed86c9
otobongfp <otobongfp@gmail.com>
2023-04-02 23:11:49 +0100

    a few changes

--- intro.go
+++ intro.go
@@ -1,25 +1,20 @@
 package main
 
-import "fmt"
-
-func parseOddsEvens(nums []int) (odds []int, evens []int) {
-
-	for _, v := range nums {
-		if v%2 == 0 {
-			evens = append(evens, v)
-		} else {
-			odds = append(odds, v)
-		}
-	}
-	return
-
-}
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
 
-	odds, evens := parseOddsEvens([]int{2, 2, 34, 23, 45, 65, 67, 89, 87, 65, 45, 11, 12, 21, 22, 44, 35, 76, 0, 5})
+	os := runtime.GOOS
 
-	fmt.Println(odds)
-	fmt.Println(evens)
+	if os == "Linux" || os == "Unix" {
+		fmt.Printf("%v \n", os)
+	} else if os == "Windows" {
+		fmt.Printf("%v \n", os)
+	} else {
+		fmt.Printf("%s \n", os)
+	}
 
 }
