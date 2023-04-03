24ff1fe24eb733827c32c3cf7a7aa84acd4a93e3
otobongfp <otobongfp@gmail.com>
2023-04-03 01:14:33 +0100

    a few changes

--- intro.go
+++ intro.go
@@ -2,28 +2,24 @@ package main
 
 import "fmt"
 
-type humanoid interface {
-	speak()
-	walk()
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
 }
-
-type person struct{ name string }
-
-func (p person) speak() { fmt.Printf("%s speaking...", p.name) }
-func (p person) walk()  { fmt.Printf("%s walking... \n", p.name) }
-
-type dog struct{}
-
-func (d dog) walk() { fmt.Printf("%s is walking...", d) }
 
 func main() {
 
-	p := person{name: "Bolt"}
-	doHumanoid(p)
+	nextSeq := intSeq()
+	fmt.Println(nextSeq())
+	fmt.Println(nextSeq())
+	fmt.Println(nextSeq())
+	fmt.Println(nextSeq())
 
-}
+	newSeq := intSeq()
+	fmt.Println(newSeq())
+	fmt.Println(newSeq())
 
-func doHumanoid(h humanoid) {
-	h.speak()
-	h.walk()
 }
